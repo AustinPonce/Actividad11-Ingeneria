ff6ccd3bd9689c7105bc0077fd10c17a7dd556cb
AustinPonce <carrilloponce11@gmail.com>
2026-05-28 23:38:56 -0600

    Primer commit del proyecto BovWeight-CR

---
+++ app/Models/Pesaje.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Observers\CrearNotificacionObserver;
+use App\Observers\VerificarPesoObserver;
+use App\Observers\AuditoriaPesajeObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+
+// atributo nativo de Laravel para asignarle los 3 observadores GoF
+#[ObservedBy([CrearNotificacionObserver::class, VerificarPesoObserver::class, AuditoriaPesajeObserver::class])]
+class Pesaje extends Model
+{
+    protected $table = 'Pesaje';
+    protected $primaryKey = 'id_pesaje';
+    public $timestamps = false;
+
+    protected $fillable = ['fecha', 'peso', 'imagen', 'sincronizado', 'arete', 'id_tipo_pesaje'];
+}
